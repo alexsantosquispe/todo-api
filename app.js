2cf6de8e79e43c00e73d33ba8dea182b3d5c0d25
Alex Santos <alexsantosquispe@gmail.com>
2025-07-06 19:44:39 -0400

    get and post todos

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const express = require('express');
-const app = express();
+const crypto = require('node:crypto');
+const todos = require('./todos.json');
 
-// Middleware to handle JSON requests
+const app = express();
+//MIddleware to handle JSON requests
 app.use(express.json());
+app.disable('x-powered-by');
+
+app.get('/todos', (req, res) => {
+  const { status } = req.query;
+
+  if (status) {
+    const todosFiltered = todos.filter(
+      (item) => item.status.toLowerCase() === status.toLowerCase()
+    );
+    return res.json(todosFiltered);
+  }
+  return res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((item) => item.id === id);
+  if (todo) return res.json(todo);
+  return res.status(404).json({ message: 'Todo not found' });
+});
+
+app.post('/todos', (req, res) => {
+  const { description, status } = req.body;
+
+  if (!description || !status) {
+    return res.status(400).json({ message: 'Missing description or status' });
+  }
+
+  const newTodo = {
+    id: crypto.randomUUID(),
+    description,
+    status
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
